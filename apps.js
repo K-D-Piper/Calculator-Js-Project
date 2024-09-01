9dad46b7191813f70e80a3ae03441ce1c0c15ebf
K-D-Piper <kirsten.piper@mscoding.org>
2024-08-31 21:03:14 -0500

    Began Tests
    Created new sections for tests. Added youtube example

--- apps.js
+++ apps.js
@@ -1,2 +1,25 @@
-let test = "Console Test Complete"
-console.log(test)
+//TESTING WHOLE UL CLASS
+let wholeTest = document.getElementById("buttNums")
+let wholeTest2 = document.getElementsByClassName("buttNums")
+let wholeTest3 = document.querySelector
+//TESTING INPUT BUTTON 
+let inputTest = document.getElementById("oneInput")
+
+//TESTING LI INPUT BUTTON
+
+//TESTING LI BUTTON 
+
+//TESTING JS SOLO 
+
+let jsTest = 
+
+
+
+/*
+THIS IS WHAT IT LOOKS LIKE IN HTML 
+<ul class = "buttNums">
+<input type="button" value=" 1 INPUT" onclick=" display.value += '1 INPUT' ">    
+<li><input type="button" value=" 1 LI BUTTON INPUT" onclick=" display.value += '1 LI BUTTON INPUT' "></li>
+<li><button> 1 LI BUTTON </button></li>
+
+*/
